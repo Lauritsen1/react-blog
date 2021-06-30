6ab80c58a349e97ca5b6a224ae642771611333f0
Lauritsen1 <43439056+Lauritsen1@users.noreply.github.com>
2021-06-30 08:53:33 +0200

    Home

---
+++ src/components/home/home.jsx
@@ -0,0 +1,16 @@
+import './home.scss';
+
+import Post from '../post/post';
+
+const Home = () => {
+
+    return (
+        <section className="post-container">
+            <Post />
+            <Post />
+            <Post />
+        </section>
+    );
+}
+
+export default Home;
